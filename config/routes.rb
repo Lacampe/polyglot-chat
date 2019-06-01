7f29c477117319d4e37e97e8c04274fc19644181
Romain Campech <romain.campech@city.ac.uk>
2019-06-01 10:31:38 +0100

    Authenticated/Unauthenticated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root to: 'conversations#home', as: :authenticated_root
+  end
+  root to: 'pages#home', as: :unauthenticated_root
+
   devise_for :users,
     path: '', path_names: { sign_up: 'signup', sign_in: 'login', sign_out: 'logout' }
 end
